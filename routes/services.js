22762eb230cec899e703e28abee7b161d5653360
Gino <lushov03@gmail.com>
2023-08-11 12:10:03 +0300

    modified:   routes/services.js

--- routes/services.js
+++ routes/services.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { models } = require('../models');
 const multer = require('multer');
-
+const path = require('path');
 const CosmetologyService = models.CosmetologyService;
 const HairdressingService = models.HairdressingService;
 const img = multer({ dest: 'img/' });
