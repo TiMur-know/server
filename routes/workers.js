0ed22b9636123edc5320b4e2c8acb0051b02c5bd
Gino <lushov03@gmail.com>
2023-08-11 10:48:30 +0300

    modified:   routes/workers.js

--- routes/workers.js
+++ routes/workers.js
@@ -12,20 +12,4 @@ router.get('/', async (req, res) => {
     res.status(500).json({ error: 'Error fetching workers' });
   }
 });
-
-router.put('/', async (req, res) => {
-  const updatedWorkers = req.body;
-
-  try {
-    for (const updatedWorker of updatedWorkers) {
-      await Worker.update(updatedWorker, { where: { id: updatedWorker.id } });
-    }
-
-    res.json({ message: 'Workers updated successfully' });
-  } catch (error) {
-    console.error('Error updating workers:', error);
-    res.status(500).json({ error: 'Error updating workers' });
-  }
-});
-
 module.exports = router;
