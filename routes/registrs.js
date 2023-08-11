2d538f6c9f831fc4b351a8c0c24feb16be90d0b2
Gino <lushov03@gmail.com>
2023-08-11 11:58:26 +0300

    modified:   routes/registrs.js

--- routes/registrs.js
+++ routes/registrs.js
@@ -7,7 +7,6 @@ const HairdressingReceipt = models.HairdressingReceipt
 const CosmetologyService = models.CosmetologyService
 const HairdressingService = models.HairdressingService
 const Client =models.Client
-console.log('work reg')
 router.get('/', async (req, res) => {
     try {
         const registrs = await Registr.findAll({
@@ -33,5 +32,56 @@ router.get('/', async (req, res) => {
       }
       
 });
+router.post("/",async(req,res)=>{
+    const { dateStart, dateEnd, hairdressingReceipts, cosmetologyReceipts } = req.body;
 
+  try {
+    const newRegistr = await Registr.create({
+      date_start: dateStart,
+      date_end: dateEnd,
+      total_cost: calculateTotalCost(hairdressingReceipts, cosmetologyReceipts),
+    });
+    if (hairdressingReceipts && hairdressingReceipts.length > 0) {
+      await HairdressingReceipts_Registr.bulkCreate(
+        hairdressingReceipts.map(receiptId => ({
+          HairdressingReceiptId: receiptId,
+          RegistrId: newRegistr.id,
+        }))
+      );
+    }
+
+    if (cosmetologyReceipts && cosmetologyReceipts.length > 0) {
+      await CosmetologyReceipts_Registr.bulkCreate(
+        cosmetologyReceipts.map(receiptId => ({
+          CosmetologyReceiptId: receiptId,
+          RegistrId: newRegistr.id,
+        }))
+      );
+    }
+
+    res.json({ message: 'New report created successfully' });
+  } catch (error) {
+    console.error('Error creating report:', error);
+    res.status(500).json({ error: 'Error creating report' });
+  }
+})
+const calculateTotalCost=async(hairdressingReceipts, cosmetologyReceipts)=> {
+    let totalCost = 0;
+  
+    if (hairdressingReceipts) {
+      for (const receiptId of hairdressingReceipts) {
+        const receipt = await HairdressingReceipt.findByPk(receiptId);
+        totalCost += receipt.total_cost;
+      }
+    }
+  
+    if (cosmetologyReceipts) {
+      for (const receiptId of cosmetologyReceipts) {
+        const receipt = await CosmetologyReceipt.findByPk(receiptId);
+        totalCost += receipt.total_cost;
+      }
+    }
+  
+    return totalCost;
+  }
 module.exports=router;
