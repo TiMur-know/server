1b58ca5349a10047bbda397ad4871d6ec8df430c
Gino <lushov03@gmail.com>
2023-08-11 19:53:01 +0300

    modified:   routes/services.js

--- routes/services.js
+++ routes/services.js
@@ -56,7 +56,38 @@ router.post('/',img.single('photo'), async(req,res)=>{
     res.status(500).json({ error: 'Error saving service' });
   }
 })
+router.put('/edit/:type/:id', img.single('photo'), async (req, res) => {
+  const { id, type } = req.params;
+  const { name, description, price, gender } = req.body;
+  let photoPath = null;
 
+  if (req.file) {
+    const imagePath = req.file.path;
+    const photoFileName = `${Date.now()}_${path.basename(imagePath)}`;
+    photoPath = `img/${type}/${photoFileName}`;
+
+    fs.renameSync(imagePath, photoPath);
+  }
+
+  try {
+    if (type === 'cosmetology') {
+      await CosmetologyService.update(
+        { name, description, price, photo: photoPath },
+        { where: { id } }
+      );
+    } else if (type === 'hairdressing') {
+      await HairdressingService.update(
+        { name, description, price, gender, photo: photoPath },
+        { where: { id } }
+      );
+    }
+
+    res.status(200).json({ message: 'Service updated successfully' });
+  } catch (error) {
+    console.error('Error updating service:', error);
+    res.status(500).json({ error: 'Error updating service' });
+  }
+});
 router.delete('/:type/:id', async (req, res) => {
     const { id, type } = req.params;
   
